fe5f25501a40bf9c8bd5392627933b472ebf4c96
kevinrhu <kevinr.hu@mail.utoronto.ca>
2023-10-23 10:18:46 -0400

    wip: added base code in empty files from last commit

--- src/use_case/navigation/NavigationInteractor.java
+++ src/use_case/navigation/NavigationInteractor.java
@@ -1,4 +1,4 @@
-package use_case;
+package use_case.navigation;
 
-public class searchMenuInteractor {
+public class NavigationInteractor {
 }
---
+++ src/use_case/player_seach/PlayerSearchInteractor.java
@@ -0,0 +1,4 @@
+package use_case.player_seach;
+
+public class PlayerSearchInteractor {
+}
