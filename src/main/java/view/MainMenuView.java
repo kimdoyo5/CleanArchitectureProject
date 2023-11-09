62a5c50f89dc2de26eabbdf2830e5402e9663ebc
giills <julia.gilley@mail.utoronto.ca>
2023-11-09 00:39:50 -0500

    finished main menu

--- src/main/java/view/MainMenuView.java
+++ src/main/java/view/MainMenuView.java
@@ -9,35 +9,78 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import interface_adapter.navigation.NavigationState;
 
 public class MainMenuView extends JPanel implements ActionListener, PropertyChangeListener {
     public final String viewName = "Main Menu";
     private final NavigationViewModel navigationViewModel;
+    private final JButton compare;
+    private final JButton IDsearch;
+    private final JButton playerSearch;
 
     // add JButtons later
     public MainMenuView(NavigationViewModel navigationViewModel, NavigationController navigationController) {
         this.navigationViewModel = navigationViewModel;
-        // this.navigationViewModel.addPropertyChangeListener(this);
+        this.navigationViewModel.addPropertyChangeListener(this);
 
         JLabel title = new JLabel("Main Menu");
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+        JPanel buttons = new JPanel();
+        compare = new JButton(NavigationViewModel.COMPARE_BUTTON_LABEL);
+        buttons.add(compare);
+        IDsearch = new JButton(NavigationViewModel.ID_SEARCH_BUTTON_LABEL);
+        buttons.add(IDsearch);
+        playerSearch = new JButton(NavigationViewModel.PLAYER_SEARCH_BUTTON_LABEL);
+        buttons.add(playerSearch);
 
-        this.setLayout(new GridLayout());
+        compare.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent evt) {
+                        if (evt.getSource().equals(compare)) {
+                            navigationController.execute("compare");
+                        }
+                    }
+                }
+        );
+
+        IDsearch.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent evt) {
+                        if (evt.getSource().equals(IDsearch)) {
+                            navigationController.execute("IDSearch");
+                        }
+                    }
+                }
+        );
+
+        playerSearch.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent evt) {
+                        if (evt.getSource().equals(compare)) {
+                            navigationController.execute("playerSearch");
+                        }
+                    }
+                }
+        );
+
+
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         this.add(title);
+        this.add(buttons);
     }
 
-   /*
+
     public void actionPerformed(ActionEvent evt) {
         System.out.println("Click " + evt.getActionCommand());
     }
 
     @Override
+    // No properties to change??
     public void propertyChange(PropertyChangeEvent evt) {
-        LoggedInState state = (LoggedInState) evt.getNewValue();
-        username.setText(state.getUsername());
+
     }
-    */
+
 
 }
