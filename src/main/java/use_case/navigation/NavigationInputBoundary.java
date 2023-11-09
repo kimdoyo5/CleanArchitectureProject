ccba3e6cb1b8cf0f73dd86f393e5661a152ae412
Gilley Julia <gilleyju@teach.cs.toronto.edu>
2023-11-08 21:56:00 -0500

    created a bunch of new files and worked on nav menu

---
+++ src/main/java/use_case/navigation/NavigationInputBoundary.java
@@ -0,0 +1,4 @@
+package main.java.use_case.navigation;
+
+public interface NavigationInputBoundry {
+}
---
+++ src/main/java/use_case/navigation/NavigationOutputBoundary.java
@@ -0,0 +1,2 @@
+package main.java.use_case.navigation;public interface NavigationOutputBoundary {
+}
