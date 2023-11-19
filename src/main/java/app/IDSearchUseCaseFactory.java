521d24f3a7e37afef12064a078fe0ac95ea15167
julia <juliamgilley@gmail.com>
2023-11-18 21:58:24 -0500

    Added database creation, added id search files

---
+++ src/main/java/app/IDSearchUseCaseFactory.java
@@ -0,0 +1,11 @@
+package main.java.app;
+
+import main.java.view.IDSearchView;
+
+public class IDSearchUseCaseFactory {
+
+    public IDSearchView create(){
+        return null;
+    }
+
+}
---
+++ src/main/java/use_case/id_search/IDSearchInputBoundary.java
@@ -0,0 +1,4 @@
+package main.java.use_case.id_search;
+
+public interface IDSearchInputBoundary {
+}
