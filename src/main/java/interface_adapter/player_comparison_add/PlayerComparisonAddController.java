2208af616479c74fc7f987170b7c41d657c976fc
Kevin Hu <kevinr.hu@mail.utoronto.ca>
2023-11-21 23:44:44 -0500

    Added player specifics for playercomparson.csv, PlayerComparisonDataAccessObject, the for the CA engine behind player_comparison_add

--- src/main/java/interface_adapter/player_comparison_add/PlayerComparisonAddController.java
+++ src/main/java/interface_adapter/player_comparison_add/PlayerComparisonAddController.java
@@ -1,6 +1,8 @@
 package main.java.interface_adapter.player_comparison_add;
 
 import main.java.use_case.player_comparison_add.PlayerComparisonAddInputBoundary;
+import main.java.entity.Player;
+import main.java.use_case.player_comparison_add.PlayerComparisonAddInputData;
 
 public class PlayerComparisonAddController {
 
@@ -9,8 +11,9 @@ public class PlayerComparisonAddController {
         this.playerComparisonAddUseCaseInteractor = playerComparisonAddUseCaseInteractor;
     }
 
-    public void execute() {
-        playerComparisonAddUseCaseInteractor.execute();
+    public void execute(Player player) {
+        PlayerComparisonAddInputData playerComparisonAddInputData = new PlayerComparisonAddInputData(player);
+        playerComparisonAddUseCaseInteractor.execute(playerComparisonAddInputData);
     }
 
 }
