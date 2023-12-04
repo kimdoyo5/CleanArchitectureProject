a5eebe3af582e6e8c6bad7ac7ead4f7dfc7ddb49
OmegamonX <jingbo.wang.0440@gmail.com>
2023-12-04 16:43:19 -0500

    Added tests to reach target coverage for interactor

--- test/main/java/interface_adapter/player_search/PlayerSearchViewModelTest.java
+++ test/main/java/interface_adapter/player_search/PlayerSearchViewModelTest.java
@@ -1,21 +1,19 @@
 import main.java.app.PlayerDataDisplayViewFactory;
 import main.java.data_access.APIDataAccessObject;
 import main.java.data_access.IDFileDataAccessObject;
-import main.java.data_access.TestPlayerAddRemoveDataAccessObject;
+import main.java.data_access.PlayerComparisonDataAccessObject;
 import main.java.entity.CommonPlayerFactory;
+import main.java.entity.Player;
 import main.java.interface_adapter.PlayerDataDisplay.PlayerDataDisplayViewModel;
 import main.java.interface_adapter.ViewManagerModel;
 import main.java.interface_adapter.navigation.MainMenuViewModel;
+import main.java.interface_adapter.player_comparison.PlayerComparisonViewModel;
 import main.java.interface_adapter.player_comparison_add.PlayerComparisonAddViewModel;
 import main.java.interface_adapter.player_search.PlayerSearchController;
 import main.java.interface_adapter.player_search.PlayerSearchPresenter;
 import main.java.interface_adapter.player_search.PlayerSearchViewModel;
 import main.java.use_case.id_search.IDSearchDataAccessInterface;
-import main.java.use_case.player_comparison_add.PlayerComparisonAddDataAccessInterface;
-import main.java.use_case.player_search.PlayerSearchDataAccessInterface;
-import main.java.use_case.player_search.PlayerSearchInputData;
-import main.java.use_case.player_search.PlayerSearchInteractor;
-import main.java.use_case.player_search.PlayerSearchOutputBoundary;
+import main.java.use_case.player_search.*;
 import main.java.view.PlayerDataDisplayView;
 import main.java.view.PlayerSearchView;
 
@@ -33,16 +31,18 @@ public class PlayerSearchTest {
     IDSearchDataAccessInterface idDataAccess = new IDFileDataAccessObject("database.csv");
     ViewManagerModel viewManagerModel = new ViewManagerModel();
 
-    PlayerComparisonAddViewModel playerComparisonAddViewModel = new PlayerComparisonAddViewModel();
-
-    PlayerComparisonAddDataAccessInterface playerComparisonAddDataAccessInterface = new TestPlayerAddRemoveDataAccessObject();
-
     PlayerSearchOutputBoundary playerSearchPresenter = new PlayerSearchPresenter(playerSearchViewModel, mainMenuViewModel, viewManagerModel, playerDataDisplayViewModel);
     PlayerSearchInteractor playerSearchInteractor = new PlayerSearchInteractor(dataAccess, playerSearchPresenter, idDataAccess);
     PlayerSearchController playerSearchController = new PlayerSearchController(playerSearchInteractor);
     JPanel playerSearchView = new PlayerSearchView(playerSearchController, playerSearchViewModel);
-    PlayerDataDisplayView playerDataDisplayView = PlayerDataDisplayViewFactory.create(playerDataDisplayViewModel,viewManagerModel,playerSearchViewModel, playerComparisonAddViewModel, playerComparisonAddDataAccessInterface);
 
+    PlayerComparisonAddViewModel playerComparisonAddViewModel = new PlayerComparisonAddViewModel();
+
+    PlayerComparisonDataAccessObject playerComparisonDataAccessObject = new PlayerComparisonDataAccessObject("playercomparison.csv", new CommonPlayerFactory());
+    PlayerDataDisplayView playerDataDisplayView = PlayerDataDisplayViewFactory.create(playerDataDisplayViewModel,viewManagerModel,playerSearchViewModel, playerComparisonAddViewModel, playerComparisonDataAccessObject);
+
+    public PlayerSearchTest() throws IOException {
+    }
 
 
     @Override
@@ -59,14 +59,64 @@ public class PlayerSearchTest {
     @org.junit.Test
     public void invalidInput() throws IOException{
         PlayerSearchInputData invalidInput = new PlayerSearchInputData(10);
-        playerSearchInteractor.execute(invalidInput);
-        assert(Objects.equals(playerSearchInteractor.getError(), "no id"));
+        PlayerSearchOutputBoundary present = new PlayerSearchOutputBoundary(){
+            @Override
+            public void prepareSuccessView(PlayerOutputData player) {
+                System.out.println("Wrong View");
+                assert (false);
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                assert (error.equals("no id"));
+            }
+
+            @Override
+            public void back() {
+                assert (false);
+            }
+
+        };
+        PlayerSearchInteractor playerSearchInteractor1 = new PlayerSearchInteractor(dataAccess ,present, idDataAccess);
+        playerSearchInteractor1.execute(invalidInput);
     }
 
     @org.junit.Test
     public void backExecute(){
         playerSearchController.execute();
         assert (viewManagerModel.getActiveView().equals(mainMenuViewModel.getViewName()));
+    }
+
+    @org.junit.Test
+    public void searchError(){
+        PlayerSearchInputData validInput = new PlayerSearchInputData(641541);
+        PlayerSearchOutputBoundary present = new PlayerSearchOutputBoundary(){
+            @Override
+            public void prepareSuccessView(PlayerOutputData player) {
+                System.out.println("Wrong View");
+                assert (false);
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                assert (error.equals("Search Error"));
+            }
+
+            @Override
+            public void back() {
+                assert (false);
+            }
+
+        };
+        PlayerSearchDataAccessInterface localData = new PlayerSearchDataAccessInterface() {
+            @Override
+            public Player search(int player_id) throws IOException {
+                IOException IOException = new IOException();
+                throw IOException;
+            }
+        };
+        PlayerSearchInteractor playerSearchInteractor1 = new PlayerSearchInteractor(localData ,present, idDataAccess);
+        playerSearchInteractor1.execute(validInput);
     }
 
 
