ac3119a29f6ee7a860681ecf72af62ef0e163ae8
julia <juliamgilley@gmail.com>
2023-12-04 21:09:50 -0500

    Changed ID search to use output data class instead of a parameter

--- src/main/java/use_case/id_search/IDSearchOutputData.java
+++ src/main/java/use_case/id_search/IDSearchOutputData.java
@@ -1,4 +1,14 @@
 package main.java.use_case.id_search;
 
+import java.util.Map;
+
 public class IDSearchOutputData {
+    private Map<String, Integer> players;
+    public IDSearchOutputData(Map<String, Integer> players){
+        this.players = players;
+    }
+
+    public Map<String, Integer> getPlayers(){
+        return players;
+    }
 }
