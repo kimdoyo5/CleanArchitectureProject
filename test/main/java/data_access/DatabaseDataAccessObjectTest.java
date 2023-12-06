b73e89511e600d09eadc070e6528db5d4988b601
OmegamonX <jingbo.wang.0440@gmail.com>
2023-12-06 13:22:33 -0500

    Added test for database creation

---
+++ test/main/java/data_access/DatabaseDataAccessObjectTest.java
@@ -0,0 +1,15 @@
+package main.java.data_access;
+
+import org.junit.Test;
+
+import java.io.File;
+
+public class DatabaseDataAccessObjectTest {
+    @org.junit.Test
+    public void createDatabase(){
+        File database = new File("database.csv");
+        assert (database.delete());
+        DatabaseDataAccessObject databaseDataAccessObject = new DatabaseDataAccessObject();
+        databaseDataAccessObject.create(2023, 2023);
+    }
+}
