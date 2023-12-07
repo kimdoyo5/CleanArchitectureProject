3bcbd88574ab3d56aaf5e0c122911b72a57f7253
julia <juliamgilley@gmail.com>
2023-12-06 22:50:26 -0500

    Added javadocs to all classes in interface_adapter/id_search

--- src/main/java/interface_adapter/id_search/IDSearchController.java
+++ src/main/java/interface_adapter/id_search/IDSearchController.java
@@ -6,17 +6,36 @@ import main.java.use_case.id_search.IDSearchInputBoundary;
 
 import java.io.IOException;
 
+/**
+ * ID Search controller, turns the user input into ID search input data, and passes
+ * this data to the interactor.
+ */
 public class IDSearchController {
     IDSearchInputBoundary idSearchInteractor;
+
+    /**
+     * Initializes the class
+     * @param idSearchInteractor the input boundary, in practice will be the
+     *                           interator
+     */
     public IDSearchController(IDSearchInputBoundary idSearchInteractor){
         this.idSearchInteractor = idSearchInteractor;
     }
 
-    public void execute(String query) throws IOException {
+    /**
+     * Behaviour for when "search" is clicked, creates IDsearch input data and
+     * calls the interactor on it
+     * @param query what the user types
+     */
+    public void execute(String query) throws IOException{
         IDSearchInputData data = new IDSearchInputData(query);
         idSearchInteractor.execute(data);
     }
 
+    /**
+     * Behaviour for when "back" is clicked, calls the interactor
+     * without a parameter
+     */
     public void execute(){
         idSearchInteractor.execute();
     }
--- src/main/java/interface_adapter/id_search/IDSearchViewModel.java
+++ src/main/java/interface_adapter/id_search/IDSearchViewModel.java
@@ -7,6 +7,11 @@ import main.java.interface_adapter.id_search.IDSearchState;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
+/**
+ * The view model for IDSearch, gives the functionality showing what has been
+ * searched and what the results of the search was. Can be updated since it
+ * extends ViewModel which means it must implement firePropertyChanged()
+ */
 public class IDSearchViewModel extends ViewModel{
     public static final String TITLE_LABEL = "ID Search View";
     public static final String SEARCH_LABEL = "Enter player name";
@@ -15,24 +20,43 @@ public class IDSearchViewModel extends ViewModel{
 
     private IDSearchState state = new IDSearchState();
 
+    /**
+     * The constructor, all attributes are set to final default values
+     */
     public IDSearchViewModel() {
         super("id search");
     }
 
+    /**
+     * Allows for the state to be changed to show new data
+     * @param state the state to change to
+     */
     public void setState(IDSearchState state) {
         this.state = state;
     }
 
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
 
+    /**
+     * Implementation of superclass, allows for the update of values in the view
+     */
     public void firePropertyChanged() {
         support.firePropertyChange("state", null, this.state);
     }
 
+    /**
+     * Allows for propertyChangeListeners to be added to the view so that
+     * data can be taken in from the text field and buttons
+     * @param listener the listener to add
+     */
     public void addPropertyChangeListener(PropertyChangeListener listener) {
         support.addPropertyChangeListener(listener);
     }
 
+    /**
+     * Gives the current state
+     * @return the current state in the view
+     */
     public IDSearchState getState() {
         return state;
     }
